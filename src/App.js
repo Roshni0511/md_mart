681bddff3c8d628b886ccb6518221874a79fb714
Roshni0511 <roshnichauhan051@gmail.com>
2025-11-07 17:22:50 +0530

    page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
+import Shop from './Pages/Shop';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   <>
   <Routes>
     <Route path='/' element={<Home/>}/>
+    <Route path='/Shop' element={<Shop/>}/>
   </Routes>
 
 
